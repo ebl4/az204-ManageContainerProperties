d4103b6342e2c5056557af6c67b49bdfee9f2496
Edson <edbdelima@gmail.com>
2023-03-26 15:52:44 -0300

    Adicionar arquivos de projeto.

---
+++ az204-ManageContainerProperties/Program.cs
@@ -0,0 +1,11 @@
+﻿using az204_ManageContainerProperties;
+using Azure.Storage.Blobs;
+
+string connectionString = "CONNECTION_STRING";
+string containerName = "wtblob" + Guid.NewGuid().ToString();
+
+BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+
+await RetrieveContainerProperties.ReadContainerPropertiesAsync(containerClient);
---
+++ az204-ManageContainerProperties/RetrieveContainerProperties.cs
@@ -0,0 +1,25 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+
+namespace az204_ManageContainerProperties
+{
+    public class RetrieveContainerProperties
+    {
+        public static async Task ReadContainerPropertiesAsync(BlobContainerClient container)
+        {
+            try
+            {
+                var properties = await container.GetPropertiesAsync();
+                Console.WriteLine($"Properties for container {container.Uri}");
+                Console.WriteLine($"Public access level: {properties.Value.PublicAccess}");
+                Console.WriteLine($"Last modified time in UTC: {properties.Value.LastModified}");
+            } catch (RequestFailedException ex) {
+                Console.WriteLine($"HTTP error code {ex.Status}: {ex.ErrorCode}");
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
